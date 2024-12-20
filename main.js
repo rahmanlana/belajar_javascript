7331b1def958c5f7804562ebd612fe3d3fc3903c
rahmanlana <rahmanlana044@gmail.com>
2024-12-20 22:13:30 +0700

    tugas_DOM_Function

--- main.js
+++ main.js
@@ -3,18 +3,50 @@
 //untuk merubah html tanpa di tag htmlnya
 
 document.title = 'maulana'
-
-
+const body = document.body
 
 const btn1 = document.getElementById('btn1')
 const btn2 = document.querySelector('.btn2')
 
-btn1.style.border = 'none'
-btn1.style.padding = '8px'
-btn1.style.fontSize = '15px'
-btn1.style.background = 'green'
-btn1.style.color = 'white'
+const defaultText = 'Klik Saya 1'
+btn1.textContent = defaultText
 
+btn2.style.border = 'none'
+btn2.style.padding = '8px'
+btn2.style.fontSize = '15px'
+btn2.style.background = 'green'
+btn2.style.color = 'white'
 
+const newText = document.createElement('p')
 
-console.log(btn1)
+//function button 1
+// function clickButton() {
+//     btn1.style.background = 'aqua'
+//     const newText = document.createElement('p')
+//     newText.textContent = "hallo lana"
+//     body.append(newText)
+
+// }
+
+// function ubahText() {
+//     btn1.textContent = 'text'
+// }
+
+// function oriText() {
+//     btn1.textContent = 'Klik saya 1'
+// }
+
+//function button 2
+
+function clicbutton2() {
+    btn2.style.background = 'aqua'
+}
+
+function munculText() {
+    newText.textContent = "Hallo Nor Rahman Maulana"
+    body.append(newText)
+}
+
+function ubahWarnaText() {
+    newText.style.color = 'aqua'
+}
