cf2b9d9234cd5dd4a66a919799d6113895b0c0bb
rahmanlana <rahmanlana044@gmail.com>
2024-12-20 10:40:28 +0700

    belajar2_js_array1

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
-let nama = 'maulana'
-let usia = 30
-let tinggiBadan = 172.5
-let beratBadan
-let pacar = 1
-
-beratBadan = 50
-
-if (pacar == null) {
-    pacar = 'belum punya'
-} else {
-    pacar = 'punya'
-}
-
-alert(
-    `nama saya ${nama} usia saya ${usia} tinggi badan saya ${tinggiBadan} berat badan saya ${beratBadan} saya ${pacar}`,
-)
-
-let saldoAwal = 50000
-let saldoTambahan = 30000
-const utang = 30000
-
-const x = 5
-const y = 7
-const z = x * y
-
-const nilaiSaldoAkhir = saldoAwal + saldoTambahan - utang
-
-alert(`saldo awal saya ${saldoAwal} & saldo tambahan yang akan saya miliki sebesar ${saldoTambahan} jadi total saldo yang saya miliki sebnyak ${nilaiSaldoAkhir}`)
-
-alert(`hasil dari ${x} x ${y} = ${z}`)
+let namaGuru = ['jodi', 'bunga', 'rahman']
+namaGuru.push('dea', 'fikri')
+namaGuru.shift()
+namaGuru.pop()
+alert(namaGuru)
