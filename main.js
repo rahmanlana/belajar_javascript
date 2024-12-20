b73974e1b820a23b96d3b96963105fc5887a7b07
rahmanlana <rahmanlana044@gmail.com>
2024-12-20 21:15:16 +0700

    DOM_sederhana_1

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
-// forloop
+//DOM
 
-// const namaGuru = ['dea', 'fikri', 'retno', 'bunga']
+//untuk merubah html tanpa di tag htmlnya
 
-// for (let i = 0; i < namaGuru.length; i++) {
-//     console.log(namaGuru[i])
-// }
+document.title = 'maulana'
 
-//tugas kecil
+console.log(document.body)
 
-//promt untuk mengetahui saldo akhir dari apa yang diinputkan oleh user
-//menentukan hari dari tanggal yang ada saat ini yang ada di komputer
-// let saldoInput = prompt(`masukan saldo anda : `)
-// console.log(saldoInput);
-// let saldoUtang = prompt(`masukan hutang anda: `)
-// console.log(saldoUtang)
-// let hasilAkhir = saldoInput - saldoUtang
-// alert(`saldo akhir anda sebesar: ${hasilAkhir}`)
+const body = document.body
+body.append('hello world')
 
-let hari = new Date().getDay()
-if (hari == 1) {
-    hari = 'senin'
-} else if (hari == 2) {
-    hari = 'selasa'
-} else if (hari == 3) {
-    hari = 'rabu'
-} else if (hari == 4) {
-    hari = 'kamis'
-} else if (hari == 5) {
-    hari = 'jumat'
-} else if (hari == 6) {
-    hari = 'sabtu'
-} else if (hari == 7) {
-    hari = 'minggu'
-}
-alert(`hari ini adalah hari ${hari}`)
-
-
-
+const h1 = document.createElement('h1')
+h1.textContent = "ini h1"
+body.append(h1)
