9b2c655ba4fe589262e7842e56fb19d6f16203df
rahmanlana <rahmanlana044@gmail.com>
2024-12-20 21:22:07 +0700

    DOM_2

--- main.js
+++ main.js
@@ -11,4 +11,13 @@ body.append('hello world')
 
 const h1 = document.createElement('h1')
 h1.textContent = "ini h1"
+
+const namaSaya = document.createElement('p')
+namaSaya.innerHTML = "<marquee>Nor Rahman maulana</marquee>"
+
+const namaKamu = document.createElement('b')
+namaKamu.innerText = "<marquee>Puspa</marquee>"
+
 body.append(h1)
+body.append(namaSaya)
+body.append(namaKamu)
