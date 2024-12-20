741b6281ee2b6f9facfda773c59acedf09045fdc
rahmanlana <rahmanlana044@gmail.com>
2024-12-20 20:56:13 +0700

    Tugas1

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // forloop
 
-const namaGuru = ['dea', 'fikri', 'retno', 'bunga']
+// const namaGuru = ['dea', 'fikri', 'retno', 'bunga']
 
-for (let i = 0; i < namaGuru.length; i++) {
-    console.log(namaGuru[i])
-}
+// for (let i = 0; i < namaGuru.length; i++) {
+//     console.log(namaGuru[i])
+// }
+
+//tugas kecil
+
+//promt untuk mengetahui saldo akhir dari apa yang diinputkan oleh user
+//menentukan hari dari tanggal yang ada saat ini yang ada di komputer
+let saldoInput = prompt(`masukan saldo anda : `)
+console.log(saldoInput);
+let saldoUtang = prompt(`masukan hutang anda: `)
+console.log(saldoUtang)
+let hasilAkhir = saldoInput - saldoUtang
+alert(`saldo akhir anda sebesar: ${hasilAkhir}`)
+
+
+
