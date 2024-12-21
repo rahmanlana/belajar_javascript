fc86cbbe4c5810e3fa0e8e81232289c971b5ff3e
rahmanlana <rahmanlana044@gmail.com>
2024-12-21 14:57:38 +0700

    array_end

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 
 //copyy tanpa merubah data array yang asli
-const arraySaya = ['alpukat', 10, {
-    semangka: function iniTomat() {
-        console.log(`ini Tomat`)
-    }
-}, ['eat', 'food']]
+const arraySaya = [{ name: 'anggur', stack: 'pisang', age: 20 }, { name: 'lana', stack: 'c++', age: 19 }]
 
-arraySaya[2].semangka()
 
-console.log(arraySaya[3][0])
+arraySaya.map((values, index) => {
+    console.log(values.name, values.stack)
+})
+
+arraySaya.filter((x) => x.age < 20).map((values) => console.log(values))
+
