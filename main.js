1966a41acd26c1f6b52f11e96e2f986ff62cddad
rahmanlana <rahmanlana044@gmail.com>
2024-12-21 14:07:15 +0700

    mencari posisi buah berada di antara

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 // array
 
-const arraySaya = ['mangga', 'nanas', 'alpukat', 'semangka']
+const arraySaya = ['mangga', 'nanas', 'alpukat', 'semangka', 'pisang']
 
 const semangka = arraySaya.includes('semangka')
 const posisiSemangka = arraySaya.indexOf('semangka')
 
 if (semangka) {
+    const indexBefore = posisiSemangka - 1
+    const indexAfeter = posisiSemangka + 1
+    const before = arraySaya[indexBefore]
+    const after = arraySaya[indexAfeter]
+    console.log(`buah semangka terletak di antara buah ${before} dan buah ${after}`)
     console.log(`posisi Semangka ada dan berada diposisi ke ${posisiSemangka}`)
 } else {
     console.log('saya tidak tau dimana posisi semngka')
