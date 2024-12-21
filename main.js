98121700c6f49fe0be831d4aaf4dcb146bc28a4a
rahmanlana <rahmanlana044@gmail.com>
2024-12-21 14:13:27 +0700

    mengetahui nilai array awal dan akhir

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ if (semangka) {
     const before = arraySaya[indexBefore]
     const after = arraySaya[indexAfeter]
     console.log(`buah semangka terletak di antara buah ${before} dan buah ${after}`)
-    console.log(`posisi Semangka ada dan berada diposisi ke ${posisiSemangka}`)
+
+    const buahAwal = arraySaya[0]
+    console.log(`buah awal adalah ${buahAwal}`)
+    const buahAkhir = arraySaya[arraySaya.length - 1]
+    console.log(`buah Terakhir adalah ${buahAkhir}`)
+
 } else {
     console.log('saya tidak tau dimana posisi semngka')
 }
