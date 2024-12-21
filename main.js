6f82b20a1553870de70b2eca94611af8129e180a
rahmanlana <rahmanlana044@gmail.com>
2024-12-21 15:30:31 +0700

    metode_function_terakhir

--- main.js
+++ main.js
@@ -18,10 +18,25 @@ const sum1 = new Function('x', 'y', 'console.log(x+y)')
 //cara 2 standart function
 function sum2(x, y) {
     console.log(x + y)
+    if (x + y == 4) {
+        console.log('berhasil')
+    }
 }
 
 //cara 3 arrow function
 const sum3 = (x, y) => {
     console.log(x + y)
 }
+
 sum3(3, 3)
+
+function random() {
+    const randomNumber = ~~(Math.random() + 300)
+    if (randomNumber > 200) {
+        console.log('wow lebih dari 200 nilai anda =', randomNumber)
+    } else {
+        console.log(randomNumber)
+    }
+}
+
+random()
