6424b88011cefb6900b0c344dd0d9efb8f71c1cc
rahmanlana <rahmanlana044@gmail.com>
2024-12-21 15:21:21 +0700

    metode function_2

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 //function
-
 function addName(fristname, lastname) {
     console.log(fristname, lastname)
 }
 
+//cara 1 new
+const sum1 = new Function('x', 'y', 'console.log(x+y)')
 
-addName('nor ', 'rahman maulana')
+//cara 2 standart function
+function sum2(x, y) {
+    console.log(x + y)
+}
+
+//cara 3 arrow function
+const sum3 = (x, y) => {
+    console.log(x + y)
+}
+
+sum1(1, 1)
+sum2(2, 2)
+sum3(3, 3)
