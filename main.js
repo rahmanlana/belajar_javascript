2f9bf5b560e3e41f8923bdee40b465e8261c5990
rahmanlana <rahmanlana044@gmail.com>
2024-12-28 15:35:47 +0700

    method_arrow_function

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
-//kata kunci this
-
-function sample() {
-    console.info(this)
-
-    function inner() {
-        console.info(this)
-    }
-
-    inner()
-}
-
-sample()
-
+//method arrow function
 
 const person = {
     name: "Eko",
-    sayHello: function (value) {
-        console.info(`Hello ${value} My Name is ${this.name}`)
+    sayHello: (name) => {
+        console.info(this);
+        console.info(`Hello ${name}, My Name is ${this.name}`)
     }
-};
+}
 
 person.sayHello("Budi")
-person.sayHello("Joko")
