d33e199499f0fca7f2c2d54b9a9a567eaa901e74
rahmanlana <rahmanlana044@gmail.com>
2024-12-28 15:14:03 +0700

    fitur_closure

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
-//global_scope_scope
 
-let counter = 0
+//kemampuan closure
+function createAdder(value) {
+    const owner = "lana"
+    function add(params) {
+        console.info(owner)
+        return value + params
+    }
 
-function hitMe() {
-    console.log(`nama saya lana `)
-    counter++
-}
-function orther() {
-    hitMe()
-}
-orther()
-hitMe()
-
-console.info(counter)
-
-function first() {
-    let firstVariable = "first"
-}
-function second() {
-    let secondVariable = "second"
+    return add
 }
 
-first()
-second()
-
-//erorrnyadalah first variable tidak di definisikan, karena variable berda di local scope, sehingga harus di akses di dalam 
-// local area scop tersebut
-//antara local scope tidak bisa di aksess
-console.info(firstVariable)
-console.info(secondVariable)
+const addTwo = createAdder(2)
