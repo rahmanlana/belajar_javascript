aa9a44624b0e9508a625c59e357c54bb52251697
rahmanlana <rahmanlana044@gmail.com>
2024-12-28 15:08:24 +0700

    local_scope

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ orther()
 hitMe()
 
 console.info(counter)
+
+function first() {
+    let firstVariable = "first"
+}
+function second() {
+    let secondVariable = "second"
+}
+
+first()
+second()
+
+//erorrnyadalah first variable tidak di definisikan, karena variable berda di local scope, sehingga harus di akses di dalam 
+// local area scop tersebut
+//antara local scope tidak bisa di aksess
+console.info(firstVariable)
+console.info(secondVariable)
