2517e7b1780852efebb94cc31678fc93cdc24945
rahmanlana <rahmanlana044@gmail.com>
2024-12-28 15:55:29 +0700

    masalah_variable_kata_kucin

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-//getter dan setter
 
-const person = {
-    firstName: "Eko",
-    lastName: "Khannedy",
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`
-    },
-    set fullName(value) {
-        const array = value.split(" ")
-        this.firstName = array[0]
-        this.lastName = array[1]
-    }
+//global scope
+let i = 20
+
+for (let i = 0; i < 10; i++) {
+    //local scope
+    console.info(`local ${i}`)
 }
 
-person.fullName = "Budi Nugraha"
-console.table(person)
+console.info(`global ${i}`)
 
-person.fullName = "Joko Morro"
-console.table(person)
 
-person.fullName = "Eko Khannedy"
-console.table(person)
+//var
+//jika membuat sebuah global scope maka variable nya akan berubah , karena seakan akan akan mengubah 
+// local merubah global nya dan ini sedikit aneg dan tidak terdefinisi
+
+var j = 20;
+
+for (var j = 0; j < 10; j++) {
+    // local scope
+    console.info(`Local ${j}`);
+}
+
+console.info(`Global ${j}`);
